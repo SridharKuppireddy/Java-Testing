ec0f2c2162667f7fac774ff224a5fda9234eb297
SridharKuppireddy <169145031+SridharKuppireddy@users.noreply.github.com>
2024-05-07 15:28:46 +0530

    Update Test.java

--- Test/src/main/java/org/example/Test.java
+++ Test/src/main/java/org/example/Test.java
@@ -1,13 +1,1 @@
-package org.example;
 
-public class Test {
-    public static void main(String[] args) {
-        char[] str={'s','r','i','d','h','a','r'};
-        for(int i=0,e=str.length-1;i<=e;++i){
-            if(Character.isLetter(str[i])&& Character.isLowerCase(str[i])){
-                str[i]=(char)(str[i]-32);
-            }
-        }
-        System.out.println(str);
-    }
-}
