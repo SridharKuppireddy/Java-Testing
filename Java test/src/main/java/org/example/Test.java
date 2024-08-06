b62b0c0e23201cd5c1454afa891c60e8b6386e0f
Sridar Kuppireddy <sridhar149k@gmail.com>
2024-08-06 18:01:21 +0530

    char changed

--- Java test/src/main/java/org/example/Test.java
+++ Java test/src/main/java/org/example/Test.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) {
-        char[] str={'s','r','i','d','h','a','r'};
+        char[] str={'s','r','i'};
         for(int i=0,e=str.length-1;i<=e;++i){
             if(Character.isLetter(str[i])&& Character.isLowerCase(str[i])){
                 str[i]=(char)(str[i]-32);
