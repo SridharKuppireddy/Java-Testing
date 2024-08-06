0e70af05376f7bf2474b197e2487dd57a9e6eab1
SridharKuppireddy <169145031+SridharKuppireddy@users.noreply.github.com>
2024-08-06 18:04:02 +0530

    Update Test.java

--- Java test/src/main/java/org/example/Test.java
+++ Java test/src/main/java/org/example/Test.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) {
-        char[] str={'s','r','i'};
+        char[] str={'m', 'a', 'n', 'i'};
         for(int i=0,e=str.length-1;i<=e;++i){
             if(Character.isLetter(str[i])&& Character.isLowerCase(str[i])){
                 str[i]=(char)(str[i]-32);
